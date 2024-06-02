2ccb468de5f8cca9c8c2e1ccc86fd1f534a07db3
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-02 18:57:35 +0200

    Icon: Logo

---
+++ frontend/src/app/components/utility/icons/icon-logo/icon-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-logo.component.html',
+  styles: ``,
+})
+export class IconLogoComponent {}
