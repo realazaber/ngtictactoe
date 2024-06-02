c86ee27c72c15af3e3e3b6169cc7914e78d4a4f7
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-02 17:41:15 +0200

    Info: Base files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#add8e6",
+        accent: "#7c8080",
+        secondary: "#dc133d",
+        thirdary: "#673bb7",
+      },
+    },
+  },
+  plugins: [],
+};
