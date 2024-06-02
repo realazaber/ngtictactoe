b4ed825b14d5082ce862f80daf03729677dd37f6
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-02 18:56:12 +0200

    Create app.routes.ts

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { GameComponent } from './pages/game/game.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: GameComponent,
+  },
+];
